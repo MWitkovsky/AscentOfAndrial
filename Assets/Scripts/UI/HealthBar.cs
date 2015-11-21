4cd4d872c7c5c855564e80c374eaf177053bd4ae
Michael Witkovsky <ishotwebkun@gmail.com>
2015-11-21 16:45:09 -0500

    Lerping health bar yo

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -9,6 +9,13 @@ public class HealthBar : MonoBehaviour
     private Image healthBar;
     private float health;
 
+    //for graphical LERP
+    private int lerpCounter;
+    private float initHealth;
+    private float displayHealth;
+    private float targetHealth;
+    private bool isHealing, isTakingDamage;
+
     // Use this for initialization
     void Start()
     {
@@ -23,30 +30,99 @@ public class HealthBar : MonoBehaviour
         {
             applyDamage(20.0f);
         }
+
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            heal(20.0f);
+        }
+
+        //Gives a more intense drain when taking damage versus a gentler fill when healing
+        if (isTakingDamage)
+        {
+            displayHealth = Mathf.Lerp(displayHealth, targetHealth, 0.25f);
+
+            if(displayHealth - targetHealth < 0.01f)
+            {
+                displayHealth = targetHealth;
+                isTakingDamage = false;
+            }
+
+            updateGraphics();
+        }
+        else if (isHealing)
+        {
+            lerpCounter++;
+            displayHealth = Mathf.Lerp(initHealth, targetHealth, lerpCounter/100.0f);
+
+            if(lerpCounter == 100)
+            {
+                displayHealth = targetHealth;
+                lerpCounter = 0;
+                isHealing = false;
+            }
+
+            updateGraphics();
+        }
     }
 
     //Takes two values and takes the resulting amount of damage
     //1.0f of rawDamage == 1% of total life
     public void applyDamage(float damage)
     {
+        initHealth = health;
         health -= damage / 100.0f;
+        targetHealth = health;
+
+        displayHealth = initHealth;
+
         if (health < 0.0f)
         {
             health = 0.0f;
+            targetHealth = health;
             //die
         }
-        updateGraphics();
+
+        isHealing = false;
+        lerpCounter = 0;
+        isTakingDamage = true;
     }
 
     public void heal(float amount)
     {
-        health += amount;
-        updateGraphics();
+        if(health < 1.00f)
+        {
+            if (!isHealing)
+            {
+                initHealth = health;
+                health += amount / 100.0f;
+                targetHealth = health;
+
+                displayHealth = initHealth;
+
+                isTakingDamage = false;
+                lerpCounter = 0;
+                isHealing = true;
+            }
+            else
+            {
+                //If heal effects gained in quick succession, compound the existing fill
+                initHealth = displayHealth;
+                health += amount / 100.0f;
+                targetHealth = health;
+
+                lerpCounter = 0;
+            }
+            //Clamp health to 100%
+            if (health > 1.00f)
+            {
+                health = 1.00f;
+                targetHealth = 1.00f;
+            }
+        }        
     }
 
-    //A lot easier than the voltage meter lol
     private void updateGraphics()
     {
-        healthBar.fillAmount = health;
+        healthBar.fillAmount = displayHealth;
     }
 }
