30ef96e18647b9be6a2d42a619cfb79d0793c8e9
Michael Witkovsky <ishotwebkun@gmail.com>
2015-12-13 19:49:39 -0500

    taking damage edit progress on level

--- Assets/Scripts/Player/ThirdPersonCharacter.cs
+++ Assets/Scripts/Player/ThirdPersonCharacter.cs
@@ -551,7 +551,8 @@ public class ThirdPersonCharacter : MonoBehaviour {
     {
         isHit = true;
         anim.SetTrigger("Hit");
-        rb.velocity = (direction * moveSpeedMultiplier) + (Vector3.up * moveSpeedMultiplier / 2.0f);
+        rb.velocity = (direction * moveSpeedMultiplier) + (Vector3.up * moveSpeedMultiplier);
+        jumpTimer = landAnimDelay;
     }
 
     public void Kill()
