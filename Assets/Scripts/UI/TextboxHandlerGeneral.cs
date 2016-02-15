53a7b6488dc1e5f85a8ed8bc5dfe55683f1a5a81
Michael Witkovsky <ishotwebkun@gmail.com>
2016-02-14 23:23:02 -0500

    general textbox package that I made for a friend

---
+++ Assets/Scripts/UI/TextboxHandlerGeneral.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TextboxHandlerGeneral : MonoBehaviour
+{
+    public AudioClip textSound;
+    public Image screenOverlay, textboxBackground, characterPortrait;
+    public Text textToPrint;
+    public bool startOpen;
+
+    private AudioSource source;
+    private float soundDelayTimer;
+    private string textQueue;
+    private int inputDelay;
+
+    void Start()
+    {
+        textToPrint = GetComponent<Text>();
+        textQueue = textToPrint.text;
+        textToPrint.text = "";
+
+        source = GetComponent<AudioSource>();
+        source.clip = textSound;
+        source.loop = false;
+
+        if (!startOpen)
+        {
+            screenOverlay.enabled = false;
+            textboxBackground.enabled = false;
+            characterPortrait.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (textQueue != "")
+        {
+            screenOverlay.enabled = true;
+            textboxBackground.enabled = true;
+            characterPortrait.enabled = true;
+            textToPrint.text += textQueue.Substring(0, 1);
+            textQueue = textQueue.Substring(1);
+
+            soundDelayTimer -= Time.deltaTime;
+            if(soundDelayTimer <= 0.0f)
+            {
+                source.Play();
+                soundDelayTimer = 0.05f;
+            }
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Fire1") && inputDelay < 0)
+        {
+            if (textQueue == "")
+            {
+                screenOverlay.enabled = false;
+                textboxBackground.enabled = false;
+                characterPortrait.enabled = false;
+                textToPrint.text = "";
+            }
+            else
+            {
+                textToPrint.text += textQueue;
+                textQueue = "";
+                source.Play();
+            }
+        }
+        else
+        {
+            inputDelay--;
+        }
+    }
+
+    public void SetTextToPrint(string textToPrint)
+    {
+        this.textToPrint.text = "";
+        textQueue = textToPrint;
+        inputDelay = 10;
+    }
+
+    public void SetCharacterPortrait(Sprite characterPortrait)
+    {
+        this.characterPortrait.sprite = characterPortrait;
+    }
+}
