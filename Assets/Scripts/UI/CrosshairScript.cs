cead0de9b1e597fa14130b72dfda4ecac2442c31
Alec Day <shirojyuu@gmail.com>
2016-04-27 18:08:15 -0400

    Added in the magical lerping crosshair!

---
+++ Assets/Scripts/UI/CrosshairScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CrosshairScript : MonoBehaviour {
+
+    private Image crosshair;
+    private Color starting;
+    private Color target;
+	// Use this for initialization
+	void Start () {
+        crosshair = GetComponent<Image>();
+        crosshair.color = Color.clear;
+        starting = Color.clear;
+        target = Color.clear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (CrossPlatformInputManager.GetButtonDown("Fire2"))
+        {
+            target = Color.white;
+           // crosshair.color = Color.white;
+        }
+
+        if (CrossPlatformInputManager.GetButtonUp("Fire2"))
+        {
+            target = Color.clear;
+           // crosshair.color = Color.clear;
+        }
+
+        crosshair.color = Color.Lerp(crosshair.color, target, Time.deltaTime * 6.0f);
+	}
+}
